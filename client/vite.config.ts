ddc6fe0c9816f7cee5307fc7bca165fdf48ca7d0
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2023-11-22 03:37:58 +0200

    Initial client/frontend commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,4 @@ export default defineConfig({
   server: {
     port: 5000,
   },
-})
+});
